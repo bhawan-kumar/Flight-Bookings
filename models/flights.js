08fcdd0a04328e26950e17755ba7da522185ed85
bhawan-kumar <chinavada.4444@gmail.com>
2022-12-20 06:04:19 +0530

    frontend + backend

---
+++ models/flights.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose")
+
+const FlightSchema = new mongoose.Schema({
+    name: String,
+    startDate: Date,
+    endDate: Date,
+    From: String,
+    To: String,
+    departureTime: Date,
+    airlines: String,
+    price: Number,
+    status: {
+        type: String,
+        default: "Active"
+    },
+    tickets: {
+        type: Number,
+        default: 60
+    },
+    passengers: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Booking"
+        }
+    ]
+})
+
+module.exports = mongoose.model("Flight", FlightSchema)
